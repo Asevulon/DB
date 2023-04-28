1458416070686e165125dd977e9a0931d6e1da12
Asevulon <Asevulon@yandex.ru>
2023-04-28 15:09:37 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //сотрудники
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form = new EmploeesForm();
+            form.ShowDialog();
+            this.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form = new TurniketForm();
+            form.ShowDialog();
+            this.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            var form = new reportForm();
+            form.ShowDialog();
+            this.Show();
+        }
+    }
+}
